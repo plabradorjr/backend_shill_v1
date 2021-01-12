f10b9f78f5b8cc270e644d22f6a6b81582f9f510
Pancho Labrador <plabradorjr@gmail.com>
2021-01-12 04:25:52 -0600

    cors url change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,8 +6,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         #note to self: that origins above is the location of your api website. like where your react is running.
 
     allow do
-        # add the domain of the production app that you will be pulling from. sample orign below:
-        origins "https://api.shill.lol"
+        # add the domain of the front-end production app on netlify.
+        origins "https://app.shill.lol"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
